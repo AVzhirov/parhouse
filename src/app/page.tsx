7c666257ad7256d7011bc63a8909bbc015246884
Z User <z@container>
2026-09-01 04:46:05 +0000

    fix: ограничить ширину фото проекта (max-w-4xl) чтобы не мылились

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1611,7 +1611,7 @@ function ProjectDetailPage({ project, onClose }: { project: typeof PROJECTS[0]; 
         </div>
       </div>
 
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8 lg:py-12">
+      <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8 py-8 lg:py-12">
         {/* Main image with gallery controls */}
         <div className="relative">
           <div
